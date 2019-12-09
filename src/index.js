be0d5a6db234d8cbc35098d700c395d4d107979f
Morgan Laupies <morgan.laupies@gmail.com>
2019-12-09 11:13:53 +0100

    add exported method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-export { default as createBrowserApp, getHistoryInstance } from './createBrowserApp';
+export {
+  default as createBrowserApp,
+  getHistoryInstance,
+  exportedGetNavigation,
+} from './createBrowserApp';
 export { default as handleServerRequest } from './handleServerRequest';
 export { default as Link } from './Link';
